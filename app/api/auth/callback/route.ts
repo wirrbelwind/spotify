633f747e5efa934393dffdd70cd5e1a07c040094
igor komendantov <komendantov2580@gmail.com>
2024-12-15 20:13:11 +0200

    complete auth callback logic

--- app/api/auth/callback/route.ts
+++
@@ -1,5 +0,0 @@
-//TODO: handle callback from spotify auth
-export const GET = (request: Request) => {
-	const url = new URL(request.url)
-	return Response.json({url})
-}
---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,74 @@
+import { cookies } from "next/headers"
+import { $axios } from "../../../../utils/$axios"
+import { redirect } from 'next/navigation'
+
+//TODO: handle callback from spotify auth
+export const GET = async (request: Request) => {
+	const cookie = await cookies()
+	const params = new URL(request.url).searchParams
+
+	const stateFromParams = params.get('state')
+	const clientSavedState = cookie.get('spotify-api:state')?.value
+
+	if (!stateFromParams) {
+		throw new Error('parameter code is null')
+	}
+
+	if (clientSavedState !== stateFromParams) {
+		throw new Error('states are not the same')
+	}
+
+	const errorParam = params.get('error')
+
+	if (errorParam) {
+		throw new Error(errorParam)
+	}
+
+	const codeParam = params.get('code')
+
+	if (!codeParam) {
+		throw new Error('code param is null')
+	}
+
+	const authHeader = btoa(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`)
+
+	try {
+		const tokensResponse = await $axios.post('https://accounts.spotify.com/api/token', {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Authorization': authHeader
+			},
+			body: {
+				grant_type: 'authorization_code',
+				code: codeParam,
+				redirect_uri: process.env.SPOTIFY_REDIRECT_URI
+			}
+		})
+
+		const tokens = tokensResponse.data as {
+			access_token: string
+			token_type: string
+			scope: string
+			expires_in: number
+			refresh_token: string
+		}
+
+		const accessTokenExpiresAt = Date.now() + tokens.expires_in
+
+		cookie.set('spotify-api:access-token', tokens.access_token)
+		cookie.set('spotify-api:access-token-expires-at', accessTokenExpiresAt.toString())
+		cookie.set('spotify-api:refresh-token', tokens.refresh_token)
+
+		const targetUrlAfterAuth = cookie.get('target-url-after-auth')?.value
+
+		if (targetUrlAfterAuth) {
+			redirect(targetUrlAfterAuth)
+		}
+		else {
+			redirect('/')
+		}
+	}
+	catch (error) {
+		throw error
+	}
+}
