7eb42f92aac8eb11921dcc2478f351fbaceb71f7
igor komendantov <komendantov2580@gmail.com>
2024-12-17 17:03:58 +0200

    delete axios response interceptor wrong code

--- src/utils/$axios.ts
+++ src/utils/$axios.ts
@@ -34,34 +34,3 @@ $axios.interceptors.request.use(async config => {
 
 	return config
 })
-
-$axios.interceptors.response.use(async response => {
-	if (response.status === 200) {
-		return response
-	}
-
-	const request = response.request
-
-	const cookie = await cookies()
-
-	let accessToken = cookie.get('spotify-api:access-token')?.value
-	let refreshToken = cookie.get('spotify-api:refresh-token')?.value
-
-	let accessTokenExpiresAt = Number(cookie.get('spotify-api:access-token-expires-at')?.value)
-
-	if (!accessToken || !refreshToken || !accessTokenExpiresAt || isNaN(accessTokenExpiresAt)) {
-		throw new Error(`no tokens | response ${accessToken} ${refreshToken} ${accessTokenExpiresAt}`)
-	}
-
-	const now = Date.now()
-
-	if (accessTokenExpiresAt <= now) {
-		const tokens = await refreshTokens(refreshToken)
-
-		if (tokens.error) {
-			throw new Error('cant refresh tokens')
-		}
-	}
-
-	return response
-})
