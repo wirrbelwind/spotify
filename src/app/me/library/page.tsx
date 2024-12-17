4e099dd2511f5853331094937b8221d91d654816
igor komendantov <komendantov2580@gmail.com>
2024-12-17 22:16:02 +0200

    create library page

---
+++ src/app/me/library/page.tsx
@@ -0,0 +1,32 @@
+import { PageObject, SimplifiedPlaylistObject } from "@/app/types"
+import { $axios } from "@/utils/$axios"
+import { Box, Card, Flex, Text } from "@mantine/core"
+import Image from "next/image"
+
+export default async function LibraryPage() {
+	const playlists = await (await $axios.get<PageObject<SimplifiedPlaylistObject>>('https://api.spotify.com/v1/me/playlists')).data
+
+	return (
+		<Box>
+			<Flex
+				rowGap="1rem"
+				columnGap="1rem"
+				wrap="wrap"
+			>
+				{playlists.items.map(playlist => (
+					<Card
+						key={playlist.id}
+					>
+						<Image
+							src={playlist.images[0].url}
+							width={160}
+							height={160}
+
+						/>
+						<Text>{playlist.name}</Text>
+					</Card>
+				))}
+			</Flex>
+		</Box>
+	)
+}
