d3ae5138d52e9b5ca87320f2114a1ebfed4273e6
igor komendantov <komendantov2580@gmail.com>
2024-12-17 17:43:03 +0200

    render top track in user profile

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,18 +1,33 @@
 import Image from "next/image";
 import { getUser } from "@/utils/getUser";
+import { $axios } from "@/utils/$axios";
+import { PageObject, TrackObject } from "../types";
 
 export default async function HomePage() {
 	const user = await getUser()
-	
+
+	const topResponse = await $axios.get<PageObject<TrackObject>>('https://api.spotify.com/v1/me/top/tracks')
+	const top = topResponse.data
+
+	const topTrack = top.items[0]
+
 	return <div>
 		{JSON.stringify(user)}
 		<h1>{user.display_name}</h1>
 		<p>{user.followers.total} followers</p>
-		<Image 
+		<Image
 			alt=""
 			src={user.images[0].url}
 			width={user.images[0].width}
 			height={user.images[0].height}
 		/>
+		<br />
+		<p>{topTrack.name}</p>
+		<p>{topTrack.artists.map(artist => artist.name)}</p>
+		<p>{topTrack.album.name}</p>
+		{topTrack.is_playable && (
+			<p>playable</p>
+		)}
+
 	</div>;
 }
