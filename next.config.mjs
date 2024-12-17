5be2397a1eaebd5fb82d8e6d78c80ba083db66c3
igor komendantov <komendantov2580@gmail.com>
2024-12-17 22:15:49 +0200

    update next.js config allow new cdn resources with images

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,22 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.scdn.co',
         pathname: '/image/**'
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'image-cdn-ak.spotifycdn.com',
+        pathname: '/image/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'mosaic.scdn.co',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'image-cdn-fa.spotifycdn.com',
+        pathname: '/image/**'
+      },
     ]
   }
 };
