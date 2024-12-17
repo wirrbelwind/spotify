e1016683cbc2c64081144ee5b42446d5634068fb
igor komendantov <komendantov2580@gmail.com>
2024-12-17 17:42:23 +0200

    use almost all scopes to api

--- src/app/api/auth/spotify-redirect/route.ts
+++ src/app/api/auth/spotify-redirect/route.ts
@@ -6,5 +6,7 @@ export const GET = async (request: Request) => {
 	const cookie = await cookies()
 	cookie.set('spotify-api:state', authState)
 
-	redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=user-read-private user-read-email&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&state=${authState}`)
+	const scopeList = `ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private`
+
+	redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=${scopeList}&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&state=${authState}`)
 }
