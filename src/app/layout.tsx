c2e7968037d9aeffec143ccfe7095e6d121c97f1
igor komendantov <komendantov2580@gmail.com>
2024-12-18 19:51:47 +0200

    migrate to next-ui: use tailwind directives

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@mantine/core/styles.css";
 import React from "react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../../theme";
+import '../global-styles.css'
 
 export const metadata = {
   title: "Mantine Next.js template",
