4b9be1afacd3aea802eda5374bd87615d64d5f54
igor komendantov <komendantov2580@gmail.com>
2024-12-18 22:02:39 +0200

    rewrite me page to next-ui

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -6,7 +6,10 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import React from "react";
 import { COOKIE_KEYS } from "@/constants";
-import { Button} from "@nextui-org/button";
+import { Button } from "@nextui-org/button";
+import { Avatar } from "@nextui-org/avatar";
+import NextImage from "next/image";
+import { Image } from "@nextui-org/image";
 
 export default async function HomePage() {
 	const user = await getUser()
@@ -21,92 +24,113 @@ export default async function HomePage() {
 	const accessToken = cookie.get(COOKIE_KEYS.ACCESS_TOKEN)?.value
 
 	return (
-	<div>
-		<Button>page button</Button>
-		 {/* <Accordion>
-      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-        123
-      </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-        123
-      </AccordionItem>
-      
-    </Accordion> */}
-
-		{/* <Flex align={"center"}>
-			<Avatar
-				variant="filled"
-				radius="9999px"
-				size="14rem"
-				src={user.images[0].url}
-				name={user.display_name}
-			/>
-			<div>
-				<Text>{user.display_name}</Text>
-				<Text>{user.followers.total} followers</Text>
+		<div>
+			<div className={`
+			flex
+			items-center
+			`}>
+				<Avatar
+					src={user.images[0].url}
+					name={user.display_name ?? '?'}
+					className="w-44 h-44 text-large"
+				/>
+				<div>
+					<p>
+						{user.display_name}
+					</p>
+					<p>
+						{user.followers.total} followers
+					</p>
+				</div>
 			</div>
-		</Flex>
 
-		<Text>Top tracks</Text>
-		<Stack
-			bg="var(--mantine-color-body)"
-			align="stretch"
-			justify="flex-start"
-			gap="xs"
-		>
-			{topTrackList.items.map((track, index) => (
-				<Flex
-					key={track.id}
-					gap="2rem"
-					align="center"
-					bd="1px solid black"
-				>
-					<Text>{index + 1}</Text>
-					<Image
-						src={track.album.images[0].url}
-						alt=""
-						width={60}
-						height={60}
-					/>
-					<div>
-						<Text>{track.name}</Text>
-						<Text>
-							{track.artists.map((artist, index) => (
-								<React.Fragment key={artist.id}>
-									<Link href={artist.href}>{artist.name}</Link>
-									{index !== track.artists.length - 1 && ', '}
-								</React.Fragment>
-							))}
-						</Text>
+			<p>Top tracks</p>
+			<div
+				className={`
+				flex
+				flex-col
+				gap-3
+				`}
+			>
+				{topTrackList.items.map((track, index) => (
+					<div
+						key={track.id}
+						gap="2rem"
+						align="center"
+						bd="1px solid black"
+						className={`
+						flex
+						gap-2
+						items-center
+						border-solid 
+						border-2 
+						border-black
+							`}
+					>
+						<p>{index + 1}</p>
+						<Image
+							src={track.album.images[0].url}
+							alt=""
+							width={60}
+							height={60}
+						/>
+						<div>
+							<p>{track.name}</p>
+							<p>
+								{track.artists.map((artist, index) => (
+									<React.Fragment key={artist.id}>
+										<Link href={artist.href}>{artist.name}</Link>
+										{index !== track.artists.length - 1 && ', '}
+									</React.Fragment>
+								))}
+							</p>
+						</div>
+						<Link href={track.album.href}>
+							{track.album.name}
+						</Link>
+
+						<p>
+							{track.duration_ms / 1000 / 60}
+						</p>
 					</div>
-					<Link href={track.album.href}>
-						{track.album.name}
-					</Link>
+				))}
+			</div>
 
-					<Text>
-						{track.duration_ms / 1000 / 60}
-					</Text>
-				</Flex>
-			))}
-		</Stack>
-
-		<Text>Top Artists</Text>
-		<Flex
-			gap="2rem"
-		>
-			{topArtistList.items.map(artist => (
-				<Card
-					key={artist.id}
-				>
-					<Image
-						src={artist.images?.[0]?.url}
-						width={180}
-						height={180}
-					/>
-					<Text>{artist.name}</Text>
-				</Card>
-			))}
-		</Flex> */}
-		<Player accessToken={accessToken} />
-	</div>);
+			<p>Top Artists</p>
+			<div className={`
+			flex
+			gap-2
+				`}>
+					{topArtistList.items.map(artist => (
+					<div
+						key={artist.id}
+					>
+						<Image
+							src={artist.images?.[0]?.url}
+							width={180}
+							height={180}
+						/>
+						<p>{artist.name}</p>
+					</div>
+					))}
+			</div>
+			{/* 
+			<Flex
+				gap="2rem"
+			>
+				{topArtistList.items.map(artist => (
+					<Card
+						key={artist.id}
+					>
+						<Image
+							src={artist.images?.[0]?.url}
+							width={180}
+							height={180}
+						/>
+						<Text>{artist.name}</Text>
+					</Card>
+				))}
+			</Flex> */}
+			<Player accessToken={accessToken} />
+		</div>);
 }
