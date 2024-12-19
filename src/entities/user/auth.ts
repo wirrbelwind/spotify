7169b8cc2b410d60554bbb35597aca709f7b7e49
igor komendantov <komendantov2580@gmail.com>
2024-12-19 13:30:23 +0200

    user: add getters/setters to auth()

--- src/entities/user/auth.ts
+++ src/entities/user/auth.ts
@@ -7,16 +7,84 @@ export const auth = async () => {
 	return {
 		get tokens() {
 			return {
-				accessToken: cookie.get(COOKIE_KEYS.ACCESS_TOKEN)?.value,
-				accessTokenExpiresAt: cookie.get(COOKIE_KEYS.ACCESS_TOKEN_EXPIRES_AT)?.value,
-				refreshToken: cookie.get(COOKIE_KEYS.REFRESH_TOKEN)?.value
+				get accessToken() {
+					return cookie.get(COOKIE_KEYS.ACCESS_TOKEN)?.value ?? null
+				},
+				set accessToken(value: string | null) {
+					if (typeof value === 'string') {
+						cookie.set(COOKIE_KEYS.ACCESS_TOKEN, value)
+						return
+					}
+					if (value === null) {
+						cookie.delete(COOKIE_KEYS.ACCESS_TOKEN)
+						return
+					}
+
+					throw new Error(`Not valid value in "auth() -> set accessToken()"`)
+				},
+				get accessTokenExpiresAt() {
+					return cookie.get(COOKIE_KEYS.ACCESS_TOKEN_EXPIRES_AT)?.value ?? null
+				},
+				set accessTokenExpiresAt(value: string | number | null) {
+					const isValidValue = typeof value === 'string' || typeof value === 'number'
+
+					if (isValidValue) {
+						cookie.set(COOKIE_KEYS.ACCESS_TOKEN_EXPIRES_AT, value.toString())
+						return
+					}
+					if (value === null) {
+						cookie.delete(COOKIE_KEYS.ACCESS_TOKEN_EXPIRES_AT)
+						return
+					}
+
+					throw new Error(`Not valid value in "auth() -> set accessTokenExpiresAt()"`)
+				},
+				get refreshToken() {
+					return cookie.get(COOKIE_KEYS.REFRESH_TOKEN)?.value ?? null
+				},
+				set refreshToken(value: string | null) {
+					if (typeof value === 'string') {
+						cookie.set(COOKIE_KEYS.REFRESH_TOKEN, value)
+						return
+					}
+					if (typeof value === null) {
+						cookie.delete(COOKIE_KEYS.REFRESH_TOKEN)
+						return
+					}
+					throw new Error(`Not valid value in "auth() -> set refreshToken()"`)
+				}
 			}
 		},
 
 		get process() {
 			return {
-				state: cookie.get(COOKIE_KEYS.STATE)?.value,
-				targetPageAfterLogin: cookie.get(COOKIE_KEYS.TARGET_PAGE_AFTER_LOGIN)?.value
+				get state() {
+					return cookie.get(COOKIE_KEYS.STATE)?.value ?? null
+				},
+				set state(value: string | null) {
+					if (typeof value === 'string') {
+						cookie.set(COOKIE_KEYS.STATE, value)
+						return
+					}
+					if (typeof value === null) {
+						cookie.delete(COOKIE_KEYS.STATE)
+						return
+					}
+					throw new Error(`Not valid value in "auth() -> set state()"`)
+				},
+				get targetPageAfterLogin() {
+					return cookie.get(COOKIE_KEYS.TARGET_PAGE_AFTER_LOGIN)?.value ?? null
+				},
+				set targetPageAfterLogin(value) {
+					if (typeof value === 'string') {
+						cookie.set(COOKIE_KEYS.TARGET_PAGE_AFTER_LOGIN, value)
+						return
+					}
+					if (typeof value === null) {
+						cookie.delete(COOKIE_KEYS.TARGET_PAGE_AFTER_LOGIN)
+						return
+					}
+					throw new Error(`Not valid value in "auth() -> set targetPageAfterLogin()"`)				}
 			}
 		}
 	}
