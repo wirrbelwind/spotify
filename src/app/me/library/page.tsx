8df64d48735f7b31d6bec5b6202dbdb580df6b6e
igor komendantov <komendantov2580@gmail.com>
2024-12-19 23:55:51 +0200

    playlist clickable

--- src/app/me/library/page.tsx
+++ src/app/me/library/page.tsx
@@ -4,6 +4,7 @@ import { $axios } from "@/utils/$axios"
 import { Image } from "@nextui-org/image";
 import { PlayButton } from "./PlayButton";
 import UserEntity from "@/entities/user";
+import Link from "next/link";
 
 export default async function LibraryPage() {
 	const playlists = await (await $axios.get<PageObject<SimplifiedPlaylistObject>>('https://api.spotify.com/v1/me/playlists')).data
@@ -22,8 +23,12 @@ export default async function LibraryPage() {
 				{playlists.items.map(playlist => (
 					<div
 						key={playlist.id}
-						className="hover:bg-slate-400 h-56 p-2 group rounded-md"
+						className="hover:bg-slate-400 h-56 p-2 group rounded-md relative"
 					>
+						<Link
+							href={`/playlist/${playlist.id}`}
+							className="w-full h-full absolute top-0 bottom-0 left-0 right-0 z-20"
+						></Link>
 						<div className="relative overflow-hidden">
 							<Image
 								src={playlist.images[0].url}
@@ -31,7 +36,7 @@ export default async function LibraryPage() {
 								// height={160}
 
 								classNames={{
-									img: "object-cover w-40 h-40",
+									img: "object-cover w-40 h-40 z-10",
 									wrapper: 'mx-auto'
 								}}
 							/>
