be28be3360f59f82aa0d57553818023d4efb20ac
igor komendantov <komendantov2580@gmail.com>
2025-01-05 18:39:06 +0200

    use TanstackQueryProvider

--- src/providers/AllProviders.tsx
+++ src/providers/AllProviders.tsx
@@ -3,13 +3,16 @@
 import { PropsWithChildren } from 'react'
 import { NextUIProvider } from './next-ui';
 import UserEntity from '@/entities/user';
+import { TanstackQueryProvider } from './tanstack-query';
 
 export const AllProviders: React.FC<PropsWithChildren> = async ({ children }) => {
 	const auth = await UserEntity.authService()
 
 	return (
 		<NextUIProvider>
+			<TanstackQueryProvider>
 				{children}
+			</TanstackQueryProvider>
 		</NextUIProvider>
 	)
 }
--- src/providers/tanstack-query.tsx
+++ src/providers/tanstack-query.tsx
@@ -36,7 +36,7 @@ function getQueryClient() {
   }
 }
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export const TanstackQueryProvider({ children }: { children: React.ReactNode }) {
   // NOTE: Avoid useState when initializing the query client if you don't
   //       have a suspense boundary between this and the code that may
   //       suspend because React will throw away the client on the initial
