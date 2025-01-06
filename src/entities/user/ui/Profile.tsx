3bee85b2126c70f3c4c28547d98c2c07f29efd62
igor komendantov <komendantov2580@gmail.com>
2025-01-06 13:26:06 +0200

    me page css

--- src/entities/user/ui/Profile.tsx
+++ src/entities/user/ui/Profile.tsx
@@ -17,17 +17,37 @@ export const Profile = () => {
 				<div className={`
 				flex
 				items-center
+				py-10
+				px-5
+				relative
 				`}>
+					{/* Blurred image on background */}
+					{/* <Image
+						src={user.data.images[0].url}
+						alt=""
+						width={500}
+						height={500}
+						className={`
+						absolute
+						top-0
+						left-0
+						w-full
+						h-full
+						object-cover
+						blur-lg
+						`}
+					/> */}
 					<Avatar
 						src={user.data.images[0].url}
 						name={user.data.display_name ?? '?'}
 						className="w-44 h-44 text-large"
 					/>
-					<div>
-						<p>
+					<div className="ml-7">
+						<p className="font-bold text-5xl">
+
 							{user.data.display_name}
 						</p>
-						<p>
+						<p className="mt-3">
 							{user.data.followers.total} followers
 						</p>
 					</div>
