a4059fbcad4e63e6bad5dbd76d563ba69cd760f2
igor komendantov <komendantov2580@gmail.com>
2025-01-17 18:45:26 +0200

    api: create regexes

--- src/shared/api/spotify-client/constants.ts
+++ src/shared/api/spotify-client/constants.ts
@@ -1,2 +1,11 @@
+// URLs
 export const DATA_API_URL = 'https://api.spotify.com/v1'
 export const AUTH_API_URL = 'https://accounts.spotify.com'
+
+// REGEXs
+export const COUNRTRY_ISO_ALPHA_2 = /^[A-Z]{2}$/
+export const SPOTIFY_URI = /^spotify:[^:]+:[^:]+$/
+export const SPOTIFY_ID = /^[A-Za-z0-9]+$/
+export const ISRC = /^[A-Z]{2}[0-9A-Z]{3}[0-9]{2}[0-9]{5}$/
+export const EAN = /^(?:\d{8}|\d{13})$/
+export const UPC = /^(?:\d{8}|\d{12})$/
