bfa3a99231da5e2ca8471ba57a248460df5d5c56
Wirbelwind <komendantov2580@gmail.com>
2025-02-05 00:55:06 +0200

    rename folder in spotify-client

--- src/shared/api/spotify-client/index.ts
+++ src/shared/api/spotify-client/index.ts
@@ -1,14 +1,14 @@
-import { checkUsersSavedTracks } from "./requests/check-user-saved-tracks";
-import { getCurrentUserPlaylists } from "./requests/get-current-user-playlists";
-import { getCurrentUserProfile } from "./requests/get-current-user-profile";
-import { getCurrentUserTopTracks } from "./requests/get-current-user-top-tracks";
-import { getPlaylist } from "./requests/get-playlist";
-import { getRecommendations } from "./requests/get-recommendations";
-import { getTokensByCode } from "./requests/get-tokens-by-code";
-import { play } from "./requests/player/play";
-import { shuffle } from "./requests/player/shuffle";
-import { refreshTokens } from "./requests/refresh-tokens";
-import { saveTracksForCurrentUser } from "./requests/save-tracks-for-current-user";
+import { checkUsersSavedTracks } from "./http-requests/check-user-saved-tracks";
+import { getCurrentUserPlaylists } from "./http-requests/get-current-user-playlists";
+import { getCurrentUserProfile } from "./http-requests/get-current-user-profile";
+import { getCurrentUserTopTracks } from "./http-requests/get-current-user-top-tracks";
+import { getPlaylist } from "./http-requests/get-playlist";
+import { getRecommendations } from "./http-requests/get-recommendations";
+import { getTokensByCode } from "./http-requests/get-tokens-by-code";
+import { play } from "./http-requests/player/play";
+import { shuffle } from "./http-requests/player/shuffle";
+import { refreshTokens } from "./http-requests/refresh-tokens";
+import { saveTracksForCurrentUser } from "./http-requests/save-tracks-for-current-user";
 
 export const spotifyClient = {
 	checkUsersSavedTracks,
