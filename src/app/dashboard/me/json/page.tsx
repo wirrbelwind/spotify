1f3bd1e438386c21d7849dd0c4590367c7518ff4
Wirbelwind <komendantov2580@gmail.com>
2025-02-06 01:10:37 +0200

    delete test page

--- src/app/dashboard/me/json/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getCurrentUser } from "@/entities/user"
-
-export default async function JsonPage() {
-	const user = await getCurrentUser()
-
-	return (
-		<p>{JSON.stringify(user)}</p>
-	)
-}
