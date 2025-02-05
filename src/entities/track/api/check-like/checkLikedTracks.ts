ca96c8ba27a4f21d4e5f795758cbf3ab9237b34f
Wirbelwind <komendantov2580@gmail.com>
2025-02-06 00:54:26 +0200

    delete outdated files

--- src/entities/track/api/check-like/checkLikedTracks.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import { spotifyClient } from "@/shared/api/spotify-client"
-
-export const checkLikedTracks = async (idList: string[]): Promise<boolean[]> => {
-	const response = await spotifyClient.checkUsersSavedTracks({ idList })
-
-	return response
-}
