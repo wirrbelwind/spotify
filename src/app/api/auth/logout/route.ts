a5b708b7c4661cab7910100a17db46ce5d6da039
Wirbelwind <komendantov2580@gmail.com>
2025-02-06 01:10:24 +0200

    delete console log

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -2,7 +2,6 @@ import { signOut } from "@/entities/user/model/signOut"
 import { redirect } from "next/navigation"
 
 export const GET = async (request: Request) => {
-	console.log(1)
 	await signOut()
 	redirect('/auth')
 }
