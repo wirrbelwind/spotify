e03ca62a8a9cc29c5ab362521442459fb813d66c
Wirbelwind <komendantov2580@gmail.com>
2025-02-09 20:03:06 +0200

    simplify util getIdFromUri

--- src/shared/lib/getIdFromUri.ts
+++ src/shared/lib/getIdFromUri.ts
@@ -1,16 +1,10 @@
-import { Entity } from '@/shared/api/spotify-types';
-
-export const getIdFromUri = (uri: string, entityType: Entity['type']) => {
+export const getIdFromUri = (uri: string) => {
 	const uriSeparator = ':'
 
 	const uriParts = uri.split(uriSeparator)
 
 	if (uriParts[0] !== 'spotify') {
 		throw new Error(`First segment of uri (${uri}) doesn't start with "spotify"`)
-	}
-
-	if (uriParts[1] !== entityType) {
-		throw new Error(`Second segment of uri (${uri}) is not equal to ${entityType}`)
 	}
 
 	const id = uriParts[2]
