7102e53952f1ad483a5d6057232614799f5fc6c4
Wirbelwind <komendantov2580@gmail.com>
2025-02-16 16:40:47 +0200

    create query client for user in DashboardHeader

--- src/widget/DashboardHeader/index.tsx
+++ src/widget/DashboardHeader/index.tsx
@@ -3,7 +3,7 @@
 import { userOptions } from "@/entities/user/model/userOptions"
 import { getBestFitImage } from "@/shared/lib/getBestFitImage"
 import { Button } from "@heroui/button"
-import { useQuery } from "@tanstack/react-query"
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query"
 import Image from "next/image"
 import { useMemo } from "react"
 import {
@@ -18,8 +18,15 @@ import { spotifyApi } from "@/shared/api/spotify-client"
 import { AccountDropdown } from "@/features/account-dropdown"
 import { SearchBar } from "@/features/search/input"
 import { Link } from "@heroui/link"
+import { makeQueryClient } from "@/shared/lib/makeQueryClient"
 
-export const DashboardHeader = () => {
+export const DashboardHeader = async () => {
+	const queryClientUser = makeQueryClient()
+
+	await queryClientUser.prefetchQuery(
+		spotifyApi.getCurrentUsersProfile.queryOptions()
+	)
+
 
 	return (
 		<div className="flex justify-between items-center">
@@ -31,8 +38,10 @@ export const DashboardHeader = () => {
 					src="/icons/home.svg"
 				/>
 			</Link>
-			<SearchBar/>
-			<AccountDropdown/>
+			<SearchBar />
+			<HydrationBoundary state={dehydrate(queryClientUser)}>
+				<AccountDropdown />
+			</HydrationBoundary>
 		</div>
 	)
 }
