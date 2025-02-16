0fa56a90e305cd6db5d2cd67d9f7b52d88361623
Wirbelwind <komendantov2580@gmail.com>
2025-02-16 16:39:12 +0200

    query: locate function makeQueryClient to /shared/lib

--- src/shared/providers/tanstack-query.tsx
+++ src/shared/providers/tanstack-query.tsx
@@ -8,41 +8,31 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { PropsWithChildren } from 'react'
+import { makeQueryClient } from '../lib/makeQueryClient'
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
-      },
-    },
-  })
-}
 
-let browserQueryClient: QueryClient | undefined = undefined
+// let browserQueryClient: QueryClient | undefined = undefined
 
-function getQueryClient() {
-  if (isServer) {
-    // Server: always make a new query client
-    return makeQueryClient()
-  } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
-    if (!browserQueryClient) browserQueryClient = makeQueryClient()
-    return browserQueryClient
-  }
-}
+// function getQueryClient() {
+//   if (isServer) {
+//     // Server: always make a new query client
+//     return makeQueryClient()
+//   } else {
+//     // Browser: make a new query client if we don't already have one
+//     // This is very important, so we don't re-make a new client if React
+//     // suspends during the initial render. This may not be needed if we
+//     // have a suspense boundary BELOW the creation of the query client
+//     if (!browserQueryClient) browserQueryClient = makeQueryClient()
+//     return browserQueryClient
+//   }
+// }
 
 export const TanstackQueryProvider: React.FC<PropsWithChildren> = ({children}) => {
   // NOTE: Avoid useState when initializing the query client if you don't
   //       have a suspense boundary between this and the code that may
   //       suspend because React will throw away the client on the initial
   //       render if it suspends and there is no boundary
-  const queryClient = getQueryClient()
+  const queryClient = makeQueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
