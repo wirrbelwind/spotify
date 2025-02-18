517b0f54a15a46b405523d228b9a75903fdd539d
Wirbelwind <komendantov2580@gmail.com>
2025-02-18 20:23:42 +0200

    search playlist: render a link to owner inside each card

--- src/features/search/results/ui/PlaylistsResults.tsx
+++ src/features/search/results/ui/PlaylistsResults.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "@tanstack/react-query";
 import { spotifyApi } from "@/shared/api/spotify-client";
 import { MediaCard } from "@/shared/ui/MediaCard";
 import { getBestFitImage } from "@/shared/lib/getBestFitImage";
+import { Link as UiLink } from "@heroui/link";
+import NextLink from "next/link";
 
 export const PlaylistsResults:FC<ResultsProps> = ({query}) => {
     const search = useQuery(
@@ -19,11 +21,21 @@ export const PlaylistsResults:FC<ResultsProps> = ({query}) => {
     return (
         <div className="grid grid-cols-5">
             {
-                search.isSuccess && search.data.playlists.items.map(playlist => (
+                search.isSuccess && search.data.playlists?.items.map(playlist => (
                     <MediaCard
                         id={playlist.id}
                         title={playlist.name}
-                        subtitle={playlist.owner.display_name ?? undefined}
+                        subtitle={
+                            playlist.owner ?
+                            () => (<UiLink 
+                                as={NextLink} 
+                                href={`/dashboard/user/${playlist.owner.id}`}
+                                >
+                                    {playlist.owner.display_name}
+                                    </UiLink>)
+                            :
+                            undefined
+                        }
                         imageUrl={getBestFitImage({
                             images: playlist.images,
                             preferredSize: {width: 150, height: 150}
