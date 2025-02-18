d34a90f306199e00f5bfbe01eacb344b6885ea53
Wirbelwind <komendantov2580@gmail.com>
2025-02-18 20:22:28 +0200

    user entity: refactor banner component hide general logic to base component

--- src/entities/user/ui/CurrentUserProfile.tsx
+++ src/entities/user/ui/CurrentUserProfile.tsx
@@ -4,71 +4,37 @@ import { Avatar } from "@heroui/avatar"
 import { spotifyApi } from "@/shared/api/spotify-client"
 import { Button } from "@heroui/button"
 import { useQueryClient } from "@tanstack/react-query"
+import { ProfileBannerBase } from "./ProfileBannerBase"
+import { getBestFitImage } from "@/shared/lib/getBestFitImage"
+import { useMemo } from "react"
 
 export const CurrentUserProfile = () => {
 	const user = spotifyApi.getCurrentUsersProfile.useQuery()
 
-	const client = useQueryClient()
+	const avatarUrl = useMemo(() => {
+		const PLACEHOLDER_IMAGE_URL = '/icons/user.svg'
+
+		if(!user.isSuccess) {
+			return PLACEHOLDER_IMAGE_URL
+		}
+
+		return getBestFitImage({
+			images: user.data.images,
+			preferredSize: {
+				width: 300,
+				height: 300
+			}
+		})?.url ?? PLACEHOLDER_IMAGE_URL
+	}, [])
 
 	return (
 		<>
 		{user.isSuccess && (
-			<div className={`
-				flex
-				items-center
-				py-10
-				px-5
-				relative
-				`}>
-					<Avatar
-						src={user.data.images[0].url}
-						name={user.data.display_name ?? '?'}
-						className="w-44 h-44 text-large"
-					/>
-					<div className="ml-7">
-						<p className="font-bold text-5xl">
-	
-							{user.data.display_name}
-						</p>
-						<p className="mt-3">
-							{user.data.followers.total} followers
-						</p>
-					</div>
-	
-					<Button onPress={() => {
-						client.setQueryData(
-							spotifyApi.getCurrentUsersProfile.queryOptions().queryKey,
-							data => {
-								if(!data) {
-									return data
-								}
-								console.log(data)
-	
-								return {
-									country: '',
-									display_name: '123',
-									email: 'email',
-									external_urls: {
-										spotify: 'sdada',
-									},
-									followers: {
-										href: 'asdsada',
-										total: 123
-									},
-									href: 'dsadsadsa',
-									id: 'id',
-									images: [],
-									product: 'premium',
-									type: 'user',
-									uri: 'uri'
-								}
-							}
-						)
-						// client.invalidateQueries({
-						// 	queryKey: spotifyApi.getCurrentUsersProfile.queryOptions().queryKey
-						// })
-					}}>Mock user name</Button>
-				</div>
+			<ProfileBannerBase
+				name={user.data.display_name ?? 'Name placeholder'}
+				followersAmount={user.data.followers.total}
+				imageUrl={avatarUrl}
+			/>
 		)}
 		</>
 	)
---
+++ src/entities/user/ui/ProfileBannerBase.tsx
@@ -0,0 +1,73 @@
+import { Avatar } from "@heroui/avatar"
+
+interface ProfileBannerBaseProps {
+    imageUrl: string
+    name: string
+    followersAmount: number
+}
+
+export const ProfileBannerBase = ({
+    name,
+    imageUrl,
+    followersAmount
+}: ProfileBannerBaseProps) => {
+
+    return (
+        <div className={`
+				flex
+				items-center
+				py-10
+				px-5
+				relative
+				`}>
+            <Avatar
+                src={imageUrl}
+                name={name}
+                className="w-44 h-44 text-large"
+            />
+            <div className="ml-7">
+                <p className="font-bold text-5xl">
+
+                    {name}
+                </p>
+                <p className="mt-3">
+                    {followersAmount} followers
+                </p>
+            </div>
+
+            {/* <Button onPress={() => {
+						client.setQueryData(
+							spotifyApi.getCurrentUsersProfile.queryOptions().queryKey,
+							data => {
+								if(!data) {
+									return data
+								}
+								console.log(data)
+	
+								return {
+									country: '',
+									display_name: '123',
+									email: 'email',
+									external_urls: {
+										spotify: 'sdada',
+									},
+									followers: {
+										href: 'asdsada',
+										total: 123
+									},
+									href: 'dsadsadsa',
+									id: 'id',
+									images: [],
+									product: 'premium',
+									type: 'user',
+									uri: 'uri'
+								}
+							}
+						)
+						// client.invalidateQueries({
+						// 	queryKey: spotifyApi.getCurrentUsersProfile.queryOptions().queryKey
+						// })
+					}}>Mock user name</Button> */}
+        </div>
+    )
+}
