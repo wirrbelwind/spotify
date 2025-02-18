87006d36f5928524b9309e2f7ae0a2ef30ae681f
Wirbelwind <komendantov2580@gmail.com>
2025-02-18 20:22:57 +0200

    user entity: create component for banner of another user

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,7 +1,10 @@
+
 export { useAudioBooksAccess } from "./model/useAudioBooksAccess";
 
 export { checkAccessToAudiobooks } from "./model/checkAccessToAudiobooks";
 
 export { CurrentUserProfile } from "./ui/CurrentUserProfile";
+export { AnotherUserProfile } from "./ui/AnotherUserProfile";
+
 export { authService } from "./model/authService";
 export { checkAuth } from "./model/checkAuth";
---
+++ src/entities/user/ui/AnotherUserProfile.tsx
@@ -0,0 +1,24 @@
+import { spotifyApi } from "@/shared/api/spotify-client"
+import { ProfileBannerBase } from "./ProfileBannerBase"
+import { getBestFitImage } from "@/shared/lib/getBestFitImage"
+
+interface AnotherUserProfileProps {
+    userId: string
+}
+
+export const AnotherUserProfile = async ({ userId }: AnotherUserProfileProps) => {
+    const user = await spotifyApi.getUsersProfile.fetch({ userId })
+
+    const avatarUrl = getBestFitImage({ 
+        images: user.images, 
+        preferredSize: { width: 300, height: 300 } 
+    })?.url ?? '/icons/user.svg'
+
+    return (
+        <ProfileBannerBase
+            name={user.display_name ?? 'Placeholder name'}
+            imageUrl={avatarUrl}
+            followersAmount={user.followers.total}
+        />
+    )
+}
