7a7acb3161caa50ba9e4794370a9e240215d433d
Wirbelwind <komendantov2580@gmail.com>
2025-02-19 21:02:13 +0200

    user top artists: set limit to 7

--- src/widget/TopArtists/ui/TopArtists.tsx
+++ src/widget/TopArtists/ui/TopArtists.tsx
@@ -8,30 +8,34 @@ interface TopArtistsProps {
 
 export const TopArtists = async () => {
     const artists = await spotifyApi.getCurrentUsersTopArtists.fetch({
-        limit: 5,
+        limit: 7,
         timeRange: 'long_term',
     })
 
     return (
-        <div className="flex gap-2 flex-wrap">
-        {
-            artists.items.map(artist => (
-                <MediaCard
-                    id={artist.id}
-                    title={artist.name}
-                    imageUrl={
-                        getBestFitImage({
-                            images: artist.images,
-                            preferredSize: {width: 250, height: 250}
-                        })?.url ?? '/icons/user.svg'
-                    }
-                    isRoundImage
-                    key={artist.id}
-                    playbackUri={artist.uri}
-                    subtitle="Artist"
-                />
-            ))
-        }
+        <div>
+            <p className="text-4xl">Top tracks of the semester</p>
+            <div className="flex gap-2 flex-wrap">
+
+                {
+                    artists.items.map(artist => (
+                        <MediaCard
+                            id={artist.id}
+                            title={artist.name}
+                            imageUrl={
+                                getBestFitImage({
+                                    images: artist.images,
+                                    preferredSize: { width: 250, height: 250 }
+                                })?.url ?? '/icons/user.svg'
+                            }
+                            isRoundImage
+                            key={artist.id}
+                            playbackUri={artist.uri}
+                            subtitle="Artist"
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 }
