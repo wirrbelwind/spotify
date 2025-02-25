17ce6ee1b09566357a6dca2dee02ea1f7ed67894
Wirbelwind <komendantov2580@gmail.com>
2025-02-25 15:40:01 +0200

    api: create request for get artist

---
+++ src/shared/api/spotify-client/sections/artists/get-artist/http-request.ts
@@ -0,0 +1,20 @@
+'use server'
+import { spotifyAxios } from "../../../axios-instance"
+import { DATA_API_URL } from "../../../constants"
+import { getParser } from "./parser"
+
+export interface FetchGetArtistArgs {
+    artistId: string
+}
+
+export const fetGetArtist = async ({artistId}: FetchGetArtistArgs) => {
+    const url = `/artists/${artistId}`
+    
+    const response = await spotifyAxios.get(url, {
+        baseURL: DATA_API_URL,
+    })
+
+    const json = response.data
+    const artist = getParser().parse(json)
+    return artist
+}
