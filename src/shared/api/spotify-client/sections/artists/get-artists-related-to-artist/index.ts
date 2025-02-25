db27734ab8a7f64b8ec66f659b74813edd590064
Wirbelwind <komendantov2580@gmail.com>
2025-02-25 20:56:20 +0200

    Revert "api: create request get-artists-related-to-artist" (deprecated endpoint)
    This reverts commit 4d746a439e78e3e020d95d9d15c40cd98d992d5e.

--- src/shared/api/spotify-client/sections/artists/get-artists-related-to-artist/index.ts
+++
@@ -1,7 +0,0 @@
-import { fetchArtistsRelatedToArtist } from "./http-request";
-import { queryOptions } from "./query";
-
-export const getArtistsRelatedToArtist = {
-    fetch: fetchArtistsRelatedToArtist,
-    queryOptions: queryOptions
-}
--- src/shared/api/spotify-client/sections/index.ts
+++ src/shared/api/spotify-client/sections/index.ts
@@ -18,4 +18,3 @@ export { getUsersPlaylists } from "./playlists/get-users-playlists";
 export { getPlaylistItems } from "./playlists/get-playlist-items";
 export { getArtist } from "./artists/get-artist";
 export { getArtistTopTracks } from "./artists/get-artist-top-tracks";
-export { getArtistsRelatedToArtist } from "./artists/get-artists-related-to-artist";
