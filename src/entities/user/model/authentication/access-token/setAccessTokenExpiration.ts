280b11f39b9192b4018e0522c18efa5ab56ded5f
Wirbelwind <komendantov2580@gmail.com>
2025-03-01 13:10:26 +0200

    user entity: change server action from arrow function to declared function

--- src/entities/user/model/authentication/access-token/setAccessTokenExpiration.ts
+++ src/entities/user/model/authentication/access-token/setAccessTokenExpiration.ts
@@ -3,7 +3,7 @@
 import { COOKIE_KEYS } from "@/entities/user/config"
 import { cookies } from "next/headers"
 
-export const setAccessTokenExpiration = async (value: number | string) => {
+export async function setAccessTokenExpiration (value: number | string) {
     const cookie = await cookies()
 
     cookie.set(COOKIE_KEYS.ACCESS_TOKEN_EXPIRES_AT, value.toString())
