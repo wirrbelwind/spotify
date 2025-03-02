14ad54402ffdee868dcf90676c577c989e880496
Wirbelwind <komendantov2580@gmail.com>
2025-03-02 14:31:09 +0200

    use @svgr/webpack in next config

--- next.config.mjs
+++ next.config.mjs
@@ -34,6 +34,22 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  }
 };
 
 export default nextConfig;
